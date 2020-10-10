d8578dc61be2f5ed5ea3515ef33969f97c1f4ea1
S.olya <solyannikolga@gmail.com>
2020-10-10 21:15:38 +0200

    Update magic_ball.rb

--- magic_ball.rb
+++ magic_ball.rb
@@ -1,20 +1,3 @@
-if Gem.win_platform?
-  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
-  Encoding.default_internal = __ENCODING__
-
-  [STDIN, STDOUT].each do |io|
-    io.set_encoding(Encoding.default_external, Encoding.default_internal)
-  end
-end
-
-puts 'Волшебный шар приветствует вас!'
-puts 'Задайте свой вопрос и шар обязательно вам ответит.'
-puts 'Однако, следовать ответу или нет - ваше решение!'
-puts
-
-user_input = STDIN.gets.chomp
-puts
-
 greetings = [
   'Привет, дорогой друг. Отвечаю на твой вопрос...',
   'Кто вопрошает, тот получит ответ:',
